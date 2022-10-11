8937e6f488f56785d70702bec987b702e0954bcf
sankardey <sankar@matrixnmedia.com>
2022-10-11 19:00:09 +0530

    home page animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,32 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+
+  styles: [`
+    .dark-modal .modal-content {
+      background-color: #292b2c;
+      color: white;
+    }
+    .dark-modal .close {
+      color: white;
+    }
+    .light-blue-backdrop {
+      background-color: #5cb3fd;
+    }
+  `],
 })
 export class HeaderComponent implements OnInit {
 
   @Output() bodyStatus = new EventEmitter<boolean>();
   @Input() menuOff: boolean = false;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  
 
   ngOnInit(): void {
   }
@@ -18,10 +34,32 @@ export class HeaderComponent implements OnInit {
   public isMenuCollapsed = true;
 
   status: boolean = false;
-clickEvent(){
-    // this.status = !this.status;
-    this.bodyStatus.emit(true);
-    this.isMenuCollapsed = this.menuOff;
-}
+  clickEvent(){
+      // this.status = !this.status;
+      this.bodyStatus.emit(true);
+      this.isMenuCollapsed = this.menuOff;
+  }
+
+  closeResult = '';
+
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
 }
+
+
